5f64b9979298d0063bdb63580f8a2ee6755d9f82
Dmitry Genkel <80839591+aAg3@users.noreply.github.com>
2023-04-11 16:26:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ def solution(x_success: int,
     p2 = y_success / y_cnt
     p = (x_success + y_success) / (x_cnt + y_cnt)
     z = (p1 - p2)/(p * (1-p) * ((1/x_cnt) + (1/y_cnt))) ** 0.5
-    p_val = norm.sf(abs(z))
+    p_val = norm.sf(z)
     
     if p_val < 0.06:
       return True
